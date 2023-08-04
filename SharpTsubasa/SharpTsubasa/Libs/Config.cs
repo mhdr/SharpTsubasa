d8f60496c8ba86fce74153ce44fe1c76e4de4cc0
Mahmood Ramzani <mahmoodramzani@gmail.com>
2023-08-04 12:23:22 +0330

    create console app

---
+++ SharpTsubasa/SharpTsubasa/Libs/Config.cs
@@ -0,0 +1,43 @@
+﻿using IniParser;
+using IniParser.Model;
+
+namespace SharpTsubasa.Libs;
+
+public class Config
+{
+    private static Config? instance;
+
+    #region ini file
+
+    public string NoxPath { get; set; }
+    public string NoxAdb { get; set; }
+
+    #endregion
+
+    #region computed
+
+    public string AdbPath { get; set; }
+
+    #endregion
+
+    private Config()
+    {
+        // Initialization code, if any
+    }
+
+    public static Config Load()
+    {
+        if (instance == null)
+        {
+            instance = new Config();
+        }
+
+        var parser = new FileIniDataParser();
+        IniData data = parser.ReadFile("Config.ini");
+        instance.NoxPath = data["Nox"]["Path"];
+        instance.NoxAdb = data["Nox"]["Adb"];
+        instance.AdbPath = Path.Combine(instance.NoxPath, instance.NoxAdb);
+
+        return instance;
+    }
+}
