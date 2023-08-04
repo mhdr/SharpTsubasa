da9dd66264589dc713ecb6f236c560991ec69128
Mahmood Ramzani <mahmoodramzani@gmail.com>
2023-08-04 12:33:07 +0330

    run adb

---
+++ SharpTsubasa/SharpTsubasa/Libs/Worker.cs
@@ -0,0 +1,29 @@
+﻿using AdvancedSharpAdbClient;
+
+namespace SharpTsubasa.Libs;
+
+public class Worker
+{
+    public static Task RunAdb(string adbPath)
+    {
+        if (!AdbServer.Instance.GetStatus().IsRunning)
+        {
+            AdbServer server = new AdbServer();
+            StartServerResult result = server.StartServer(adbPath, false);
+            if (result != StartServerResult.Started)
+            {
+                Console.WriteLine("Can't start adb server");
+            }
+            else
+            {
+                Console.WriteLine("Adb server is running...");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Adb server is running...");
+        }
+
+        return Task.CompletedTask;
+    }
+}
--- SharpTsubasa/SharpTsubasa/Program.cs
+++ SharpTsubasa/SharpTsubasa/Program.cs
@@ -1,6 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using AdvancedSharpAdbClient;
 using SharpTsubasa.Libs;
 
 var config = Config.Load();
-Console.WriteLine(config.AdbPath);
+
+await Worker.RunAdb(config.AdbPath);
